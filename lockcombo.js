5c3bcb0ed7837966853be64d9d3f72010a8e2bc1
Dylan Salay <dylansalay@Dylans-MacBook-Pro.local>
2019-12-09 11:49:32 -0800

    Finished Game Logic and Styling

--- lockcombo.js
+++ lockcombo.js
@@ -1,4 +1,55 @@
 $(document).ready(function(){
+    $('#output').hide();
+    $('#start').click(startGame)
+    $('#checkLock').click(openLock);
+    var ourSecretNum = '';
+
+    function openLock(){
+        var win = 0;
+        for(x=0; x < $('input[type="number"]').length; x++){
+            var guessNum = $('input[type="number"]').eq(x);
+            var result = checkNumber(guessNum.val(),ourSecretNum[x])
+            guessNum.css({
+                backgroundColor: result.backgrd
+            })
+            if (result.checker) {
+                win++
+            }
+        }
+        if(win === 3){
+            $('#start').show();
+            $('small').html('You got it <br> ' + ourSecretNum )
+        }
+    }
+
+    function checkNumber(a,b){
+        var response = {};
+        if(a < b){
+            response.checker = false
+            response.backgrd = 'red'
+        }
+        if(a > b){
+            response.checker = false
+            response.backgrd = 'blue'
+        }
+        if(a == b){
+            response.checker = true
+            response.backgrd = 'green'
+        }
+        return response 
+    }
+
+    function startGame(){
+        ourSecretNum = Math.floor(Math.random()*(999-100+1)+100).toString();
+        $('#start').hide();
+        $('#output').show();
+        for(x=0;x<$('input[type="number"]').length;x++){
+            $('input[type="number"]').eq(x).val('5');
+        }
+    }
+
+    $('small').html('Red Background: Too Low <br> Blue Background: Too high');
+
     $('input[type="number"]').css({
         color:'white',
         fontSize:'3em',
@@ -31,7 +82,8 @@ $(document).ready(function(){
         width:'300px',
         padding:'15px',
         border:'1px solid black',
-        textAlign:'center'
+        textAlign:'center',
+        margin:'auto'
     })
 
     $('small').css({
