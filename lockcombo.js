b387293543a2cb1b3f720d1c545c2f55c0bd80e4
Dylan Salay <dylansalay@Dylans-MacBook-Pro.local>
2019-12-08 18:15:13 -0800

    first commit

---
+++ lockcombo.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+    $('input[type="number"]').css({
+        color:'white',
+        fontSize:'3em',
+        width:'60px',
+        border:'1px solid black',
+        backgroundColor:'black'
+    })
+
+    $('#start').css({
+        backgroundColor:'black',
+        color:'white',
+        fontSize:'2em',
+        width:'60px',
+        padding:'15px',
+        border:'1px solid black',
+    })
+
+    $('.btn').css({
+        backgroundColor:'black',
+        color:'white',
+        width:'160px',
+        fontSize:'1.5em',
+        padding:'15px',
+        margin:'25px auto 0px',
+        border:'1px solid black',
+        textAlign:'center'
+    })
+    $('#output').css({
+        backgroundColor:'#333',
+        width:'300px',
+        padding:'15px',
+        border:'1px solid black',
+        textAlign:'center'
+    })
+
+    $('small').css({
+        color:'white',
+        fontSize:'1.5em',
+        padding:'15px',
+        marginBottom:'15px',
+        display:'block'
+    })
+
+
+})
